f8a732ff3e6d8a437aa9aae4a22095030a8c0292
Nikhil Kulkarni <nikhilkulkarni2378@hera3.ece.tamu.edu>
2023-07-19 08:07:25 -0500

    Adding 2&3 assignments

--- squarewavegen.v
+++ squarewavegen.v
@@ -1,14 +1,18 @@
-//This is second assignment from Udemy courseware. The task
-//is to generate a 9MHz square wave with timescale 1ns  and
-//precision for 3 decimals.
+//This is second and third assignment from Udemy courseware
+//The task is to generate a 9MHz square wave with timescale
+//1ns  and precision for 3 decimals.
 
 `timescale 1ns/1ps;
 module squarewavegen;
   reg sclk;
+  reg clk25;
   always #55.555 sclk = ~sclk;
+  always #20.00 clk25 = ~clk25;
+ 
   initial
   begin
     sclk = 1'b0;
+    clk25 = 1'b0;
   end
 
   initial
