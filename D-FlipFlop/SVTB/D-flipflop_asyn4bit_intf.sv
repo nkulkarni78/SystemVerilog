322c3ef2fb57ac8a50616674a864d14235f437a5
Nikhil Kulkarni <nikhilkulkarni2378@n01-zeus.olympus.ece.tamu.edu>
2023-12-21 18:48:37 -0600

    D-flipflop Interface for self checking tb V3

--- D-FlipFlop/SVTB/D-flipflop_asyn4bit_intf.sv
+++ D-FlipFlop/SVTB/D-flipflop_asyn4bit_intf.sv
@@ -1,14 +1,11 @@
 //Below code is for 4-bit dff with active low asynchronous reset
 
-module dff(d,q,clk,reset);
-    input [3:0]d;
-    input clk,reset;
-    output reg [3:0]q;
-    always_ff@(posedge clk, negedge reset)
+module dff(d_intf dintf);
+    always_ff@(posedge dintf.clock, negedge dintf.reset)
     begin
-        if(!reset)
-            q <= 4'b0;
+        if(!dintf.reset)
+            dintf.q <= 4'b0;
         else
-            q <= d;
+            dintf.q <= dintf.d;
     end
 endmodule
