fd26227c9d7aea0101fcbf94a37398b4aecee91f
Nikhil Kulkarni <nikhilkulkarni2378@n01-zeus.olympus.ece.tamu.edu>
2023-12-21 18:47:59 -0600

    D-flipflop self checking testbench top V2

--- D-FlipFlop/SVTB/D-flipflop_4bit_selftb_v2_top.sv
+++ D-FlipFlop/SVTB/D-flipflop_4bit_selftb_v2_top.sv
@@ -7,16 +7,16 @@ module top;
   d_intf intf_inst();
 
   // DUT instance
-  dff dff_inst(.d(intf_inst.tb_d),
-               .q(intf_inst.tb_q),
+  dff dff_inst(.d(intf_inst.d),
+               .q(intf_inst.q),
                .clk(intf_inst.clock),
-               .reset(intf_inst.tb_reset));
+               .reset(intf_inst.reset));
 
   // program block instance for stimuli generation
   dff_tb dff_tb_inst(.tb_clk(intf_inst.clock),
-                     .tb_reset(intf_inst.tb_reset),
-                     .tb_d(intf_inst.tb_d),
-                     .tb_q(intf_inst.tb_q));
+                     .tb_reset(intf_inst.reset),
+                     .tb_d(intf_inst.d),
+                     .tb_q(intf_inst.q));
 
   initial intf_inst.clock = 1'b0;
 
