a70e5bb7cb971849eec14d6495101afb83594005
Nikhil Kulkarni <nikhilkulkarni2378@n01-zeus.olympus.ece.tamu.edu>
2024-02-21 15:33:01 -0600

    Updating Memory design with Intf ports

--- Memory/memory.sv
+++ Memory/memory.sv
@@ -5,22 +5,61 @@
 // nullified. Since there are 4096 locations we need 12 bits for address. We
 // shall design positive edge triggered active high synchronous reset memory.
 
-
-module memory(readData, writeData, readEnable, WriteEnable, clock, reset);
-  //port declarations with parameters that can be changed dynamically
+module memory#(parameter ADDR_SIZE=4096)(mem_intf dut_intf);
+  // port declarations with parameters that can be changed dynamically
   parameter DATA_SIZE = 8;
-  parameter ADDR_SIZE = 4096;
+  parameter ADDR_WIDTH = 12;
   
+  // input clock, reset;
+  // input readEnable;
+  // input writeEnable;
+
   // since you write data into memory it is input
-  input [DATA_SIZE-1:0]writeData;
-  input clock, reset, readEnable, writeEnable;
+  // input [DATA_SIZE-1:0]dut_intf.writeData_in;
+
+  // address to read or write data
+  // input [ADDR_WIDTH-1:0]dut_rwAddr;
+
   // since you read data from memory it is output
-  output reg [DATA_SIZE-1:0]readData;
+  // output [DATA_SIZE-1:0]readData_out;
+
+  // provides acknowledgement for successful write/read operation
+  // output reg ack;
+
+  // intermediate storage element to store read data
+  reg [DATA_SIZE-1:0]readData;
+
+  // memory bank for 4096 locations
   reg [DATA_SIZE-1:0]Mem[ADDR_SIZE-1:0];
 
-  always_ff(posedge clock)
-  begin
-    if()
-  end
+  // iterating element for all memory locations
+  integer i;
 
+  // output data is high impedence if writeEnable is high
+  assign dut_intf.readData_out = (dut_intf.readEnable) ? readData: 8'bzzzz_zzzz;
+
+  always_ff@(posedge dut_intf.clock)
+  begin
+    if(dut_intf.reset)
+    begin
+      // reset all the memory locations with z as its value signifying nothing
+      for(i=0; i<ADDR_SIZE; i++)
+      begin
+        Mem[i] <= 8'bzzzz_zzzz;
+      end
+    end
+    else
+    begin
+      if(dut_intf.writeEnable && !dut_intf.readEnable)
+      begin
+       Mem[dut_intf.rwAddr] <= dut_intf.writeData_in;
+       dut_intf.ack = 1'b1;
+      end
+      if(dut_intf.readEnable && !dut_intf.writeEnable)
+      begin
+        readData <= Mem[dut_intf.rwAddr];
+        dut_intf.ack <= 1'bx;
+      end
+    end
+  end
 endmodule
